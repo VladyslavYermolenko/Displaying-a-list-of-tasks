4d6bf28b53cbf488dc43e54e8802995111f97798
VladyslavYermolenko <104897591+VladyslavYermolenko@users.noreply.github.com>
2022-06-06 14:57:23 +0300

    Added the ability to display and add tasks to the TodoList. Removed page theme change. Improved design.

--- assets/js/popUp.js
+++ assets/js/popUp.js
@@ -2,13 +2,6 @@ const popUp = document.getElementById('pop-up');
 const openBtn = document.getElementById('button-add-new-task');
 const closeBtn = document.getElementsByClassName('close')[0];
 
-window.onclick = function (event) {
-    // if (event.target == popUp) {
-    //     popUp.style.display = "none";
-    // }
-    console.log('test');
-}
-
 openBtn.onclick = function () {
     popUp.style.display = 'block';
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,126 @@
-function add() {
+const tasks = [
+  {
+    id: 1,
+    name: "Task 1",
+    description: "Description",
+    done: false,
+    due_date: "",
+  },
+  {
+    id: 2,
+    name: "Task 2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt fugit, quis voluptates minima exercitationem iusto aliquid sapiente! Neque ut quo labore at ex facilis aliquid doloribus voluptatibus. Delectus, voluptatibus amet.",
+    done: true,
+    due_date: new Date("2022-06-01"),
+  },
+  {
+    id: 3,
+    name: "Task 3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt fugit, quis voluptates minima exercitationem iusto aliquid sapiente! Neque ut quo labore at ex facilis aliquid doloribus voluptatibus. Delectus, voluptatibus amet.",
+    done: false,
+    due_date: new Date("2022-01-15"),
+  },
+  {
+    id: 4,
+    name: "Task 4",
+    description: "",
+    done: true,
+    due_date: new Date("2022-09-21"),
+  },
+  {
+    id: 5,
+    name: "Task Task Task Task Task Task Task Task Task 5",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt fugit, quis voluptates minima exercitationem iusto aliquid sapiente! Neque ut quo labore at ex facilis aliquid doloribus voluptatibus. Delectus, voluptatibus amet.",
+    done: false,
+    due_date: new Date("2021-11-05"),
+  },
+];
 
+const increment = (init = tasks.reduce((prev, cur) => prev['id'] > cur['id'] ? prev : cur['id'])) => () => ++init;
+const primaryKey = increment();
+
+function checkedOrNot(isDone) {
+  if (isDone) {
+    return "checked";
+  }
+  return "";
 }
 
-function mode() {
-    const body = document.querySelector('body');
-    const themeMode = document.querySelector('#themeMode');
-    const allContainers = document.querySelectorAll('#container');  
-
-    if (themeMode.classList.contains('fa-sun')) {
-        themeMode.classList.remove('fa-sun');
-        themeMode.classList.add('fa-moon');
-
-        body.classList.remove('background-dark');
-        body.classList.add('background-light');
-
-        [].forEach.call(allContainers, function (container) {
-          container.classList.remove("container-dark");
-          container.classList.add("container-light");
-        });
-    }
-    else {
-        themeMode.classList.remove('fa-moon');
-        themeMode.classList.add('fa-sun');
-
-        body.classList.remove('background-light');
-        body.classList.add('background-dark');
-
-        [].forEach.call(allContainers, function (container) {
-          container.classList.remove("container-light");
-          container.classList.add("container-dark");
-        });
-    }
+function addNewTask() {
+  const tasks_list = document.getElementById("tasks-list");
+  const textBox_name = document.getElementById('pop-up_name');
+  const textBox_description = document.getElementById('pop-up_description');
+  const date_dueDate = document.getElementById('pop-up_due-date');
+  
+  if (date_dueDate.value) {
+    tasks_list.innerHTML += `<li>
+  <span>
+      <input type="checkbox" />
+      <h3>${textBox_name.value}</h3>
+  </span>
+  <p class="due-date">Due Date: ${new Date(date_dueDate.value).toLocaleDateString()}</p>
+  <p class="description">${textBox_description.value}</p>
+</li>
+`;
+  } else {
+    tasks_list.innerHTML += `<li>
+  <span>
+      <input type="checkbox" />
+      <h3>${textBox_name.value}</h3>
+  </span>
+  <p class="description">${textBox_description.value}</p>
+</li>
+`;
+  }
+  tasks.push({
+    id: primaryKey,
+    name: textBox_name.value,
+    description: textBox_description.value,
+    done: false,
+    due_date: date_dueDate.value
+  });
 }
+
+const button_addNewTask = document.getElementById('pop-up_add-new-task');
+button_addNewTask.addEventListener('click', function clickNewTaskButton(event) {
+  event.preventDefault();
+  const textBox_name = document.getElementById('pop-up_name');
+  
+  if (textBox_name.value) {
+    addNewTask();
+  }
+});
+
+function addDefaultTasks() {
+  const tasks_list = document.getElementById("tasks-list");
+  function addTaskInContainer(object) {
+    const { id, name, description, done, due_date } = object;
+    if (due_date) {
+      tasks_list.innerHTML += `<li>
+    <span>
+        <input type="checkbox" ${checkedOrNot(done)} />
+        <h3>${name}</h3>
+    </span>
+    <p class="due-date">Due Date: ${new Date(due_date).toLocaleDateString()}</p>
+    <p class="description">${description}</p>
+  </li>
+  `;
+    } else {
+      tasks_list.innerHTML += `<li>
+    <span>
+        <input type="checkbox" ${checkedOrNot(done)} />
+        <h3>${name}</h3>
+    </span>
+    <p class="description">${description}</p>
+  </li>
+  `;
+    }
+  }
+
+  tasks.forEach(addTaskInContainer);
+}
+
+addDefaultTasks();
